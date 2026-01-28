219141e35076618ff6886335736cd711d70f782b
554961 <554961@escg.ac.uk>
2026-01-28 11:12:15 +0000

    register and footer start

--- customer/register/index.php
+++ customer/register/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>register</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></head>
     <style>
         form
@@ -20,7 +20,7 @@
 <body>
     <!-- get navbar -->
     <?php require_once $_SERVER["DOCUMENT_ROOT"] . '\leveluploot\templates\navbar.php' ?>
-
+    
     <h2 class="alert alert-info" style="text-align:center;">Register Here</h2>
     <form method="POST">
         <div class="form-group">
@@ -31,11 +31,14 @@
             <label>Password:</label>
             <input type="password" name="password" class="form-control">
         </div>
-
+        
         <div class="form-group">
             <button type="submit">Submit</button>
         </div>
     </form>
-
+    
+    
+    <!-- get footer -->
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . '\leveluploot\templates\footer.php' ?>
 </body>
 </html>
---
+++ templates/footer.php
@@ -0,0 +1,17 @@
+<style>
+    .gradient-text 
+    {
+    background: linear-gradient(90deg, #ff6a00, #ee0979);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text; /* for newer browsers */
+    }
+
+</style>
+
+<footer style="background: linear-gradient(315deg, #8358B2, #BC63FF);">
+    <div class="container-fluid">
+    <h1>a</h1>
+    </div>
+    <br>
+</footer>
