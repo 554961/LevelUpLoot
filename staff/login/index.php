2e6493a5ed089a80bdd8ffe3d96cadec79320ff3
554961 <554961@escg.ac.uk>
2026-01-28 09:31:23 +0000

    adding navbar template and structure of all pages

---
+++ staff/login/index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></head>
+<body>
+    <!-- get navbar -->
+    <?php require_once __DIR__ . "/templates/navbar.php" ?>
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/leveluploot/">LevelUp Loot</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="/leveluploot/products">Product</a></li>
+            <li><a href="/leveluploot/support">Support</a></li>
+            <li><a href="/leveluploot/about-us">About Us</a></li>
+            <li><a href="/leveluploot/wishlist">Your Wishlist</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+    </div>
+</nav>
